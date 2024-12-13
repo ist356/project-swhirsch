6544271591b3544b97b5d4a74534474013f1b2f9
swhirsch <swhirsch@syr.edu>
2024-12-13 13:52:37 -0500

    commit

--- code/test.py
+++
@@ -1,9 +0,0 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(10, 6))
-sns.histplot(df['Age'], bins=10, kde=True)
-plt.title("Distribution of Ages in Cleaned Passing Data")
-plt.xlabel("Age")
-plt.ylabel("Frequency")
-plt.show()
